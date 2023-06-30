3f3653cdb6fb549c5635173692a5950d26173504
Maximo Lopez <7888441+mxlopez25@users.noreply.github.com>
2023-06-30 01:25:35 -0400

    StockRepositoryImplementation.kt added

---
+++ app/src/main/java/com/plcoding/stockmarketapp/data/repository/StockRepositoryImplementation.kt
@@ -0,0 +1,41 @@
+package com.plcoding.stockmarketapp.data.repository
+
+import com.plcoding.stockmarketapp.data.local.StockDatabase
+import com.plcoding.stockmarketapp.data.mapper.toCompanyListing
+import com.plcoding.stockmarketapp.data.remote.StockApi
+import com.plcoding.stockmarketapp.domain.model.CompanyListing
+import com.plcoding.stockmarketapp.domain.repository.StockRepository
+import com.plcoding.stockmarketapp.util.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class StockRepositoryImplementation @Inject constructor(
+    val api: StockApi,
+    val db: StockDatabase
+) : StockRepository {
+
+    private val dao = db.dao
+
+    override suspend fun getCompanyList(
+        fetchFromRemote: Boolean,
+        query: String
+    ): Flow<Resource<List<CompanyListing>>> {
+        return flow {
+            emit(Resource.Loading(isLoading = true))
+            val localListing = dao.searchCompanyListing(query)
+            emit(Resource.Success(
+                data = localListing.map { it.toCompanyListing() }
+            ))
+            val isDbEmpty = localListing.isEmpty() && query.isBlank()
+            val shouldJustLoadFromCache = !isDbEmpty && !fetchFromRemote
+            if(shouldJustLoadFromCache) {
+                emit(Resource.Loading(false))
+                return@flow
+            }
+        }
+    }
+
+}
